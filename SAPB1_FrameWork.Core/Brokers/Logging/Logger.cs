f39cf35db632a7b1e93e8dfdddda27056d46fff1
Sergio Tenza Lopez <sergeix77@gmail.com>
2021-11-27 18:25:43 +0100

    TEST: Core.Brokers.Logging Pass

--- SAPB1_FrameWork.Core/Brokers/Logging/Logger.cs
+++ SAPB1_FrameWork.Core/Brokers/Logging/Logger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace SAPB1_FrameWork.Core.Brokers.Logging
     {
         public void Log(string message)
         {
-            throw new NotImplementedException();
+            Debug.Write($"Evento del sistema {message}");
         }
     }
 }
