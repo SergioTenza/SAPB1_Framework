119900d7cca213aff2de104fe03bf0ee2d5a363d
Sergio Tenza Lopez <sergeix77@gmail.com>
2021-11-27 18:10:03 +0100

    INFRA: Test project added

---
+++ SAPB1_FrameWork.Core.Tests/Core.Services.Connection.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace SAPB1_FrameWork.Core.Tests
+{
+    public class Service_Connection_Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ SAPB1_FrameWork.Core.Tests/Empty.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace SAPB1_FrameWork.Core.Tests
+{
+    public class Empty
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
